27ed8e6ff8fc94866c44c79c3df223a2a20d6db5
junior <zukisanante@gmail.com>
2019-11-29 16:53:31 +0100

    database created

---
+++ config/database.config.js
@@ -0,0 +1,4 @@
+//  DATABASE CONNECT
+module.exports = {
+    url: 'mongodb//localhost:27017/easy-notes'
+}
--- server.js
+++ server.js
@@ -15,6 +15,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // PARSE REQUESTS OF CONTENT-TYPE - APPLICATION/JSON
 app.use(bodyParser.json());
 
+// CONFIGURING THE DATABASE
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// CONNECTING TO THE DATABASE
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 // DEFINE A SIMPLE ROUTE
 app.get('/', (reg, res) => {
     res.json({ 'message': 'Welcome to our Job Platform!' });
