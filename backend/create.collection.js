1f35110f3273d456aad995754ad7b573846a4a11
junior <zukisanante@gmail.com>
2019-12-02 16:54:19 +0100

    update delete insert done

--- backend/create.collection.js
+++ backend/create.collection.js
@@ -4,10 +4,10 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("JOBS");
+    var dbo = db.db("VACANIES");
 
     // CREATE
-    dbo.createCollection("customers", function(err, res) {
+    dbo.createCollection("vacancies", function(err, res) {
         if (err) throw err;
         console.log("Collection created!");
         db.close();
@@ -15,9 +15,9 @@ MongoClient.connect(url, function(err, db) {
 });
 
 // INSERT DATA TO DATABASE
-MongoClient.connect(url, function(err, db) {
+/* MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("JOBS");
+    var dbo = db.db("VACANIES");
     var myobj = [
         { _id: 1, title: 'web developer', description: 'programming', skills: 'html, css', education: 'hbo', contact: '039494', company: 'panda' },
 
@@ -28,22 +28,49 @@ MongoClient.connect(url, function(err, db) {
         { _id: 4, title: 'sales', description: 'sell', skills: 'sales and marketing', education: 'hbo', contact: '676767', company: 'purple panda' }
 
     ];
-    dbo.collection("customers").find({}).toArray(function(err, result) {
+    dbo.collection("vacancies").insertMany(myobj, function(err, res) {
         if (err) throw err;
-        console.log(result);
+        console.log("Number of documents inserted: " + res.insertedCount);
         db.close();
     });
-});
+}); */
 
 // FIND DOCUMENT 
-MongoClient.connect(url, function(err, db) {
+/* MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("JOBS");
-    dbo.collection("customers").find({}).toArray(function(err, result) {
+    var dbo = db.db("VACANIES");
+    dbo.collection("vacancies").find({}).toArray(function(err, result) {
         // TO BE INSERTED AFTER .find({}, { projection: { _id: 0 } })
         if (err) throw err;
         //console.log(result[2].title);
         console.log(result);
+        db.close();
+    });
+}); */
+
+// QUERY
+/* s */
+
+// DELETE ONE DOCUMENT
+/* MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("VACANCIES");
+    var myquery = { title: 'sales' };
+    dbo.collection("vacancies").deleteOne(myquery, function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        db.close();
+    });
+}); */
+// UPDATE DOCUMENT
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("VACANCIES");
+    var myquery = { title: "sales" };
+    var newvalues = { $set: { title: "engineer", description: "construct and design" } };
+    dbo.collection("vacanies").updateOne(myquery, newvalues, function(err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
         db.close();
     });
 });
